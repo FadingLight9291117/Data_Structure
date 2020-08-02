b5a084beadfe1a7a4a2c5c2616adf69dcc0c563d
FadingLight <FadingLight9291117@126.com>
2020-08-02 16:55:11 +0800

    add sort

--- sort/selectSort.py
+++ sort/selectSort.py
@@ -2,12 +2,13 @@
 # selectSort.py-选择排序
 
 def selectSort(arr):
-	for i in range(len(arr) - 1):
-		min = i
-		for j in range(i, len(arr)): # 选一个最小的
-			min = j if arr[j] < arr[min] else min
-		arr[i], arr[min] = arr[min], arr[i] # 交换元素
-	return arr
+    for i in range(len(arr) - 1):
+        min = i
+        for j in range(i, len(arr)):  # 选一个最小的
+            min = j if arr[j] < arr[min] else min
+        arr[i], arr[min] = arr[min], arr[i]  # 交换元素
+    return arr
+
 
 # 测试
 arr = [-2, 21, 3, 23, 10, -3, 0, -1]
